ad484db63da7f3254449adfb276f80f55a27c5ed
Gilvan Lima <gilvan.lima@mindera.com>
2022-01-11 23:25:57 +0000

    primeiro commit da branch task-1

---
+++ CalculoDeMediaApp/CalculoDeMediaApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculoDeMediaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
